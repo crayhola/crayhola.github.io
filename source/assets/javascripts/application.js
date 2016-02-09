bd4136e31ad935f1f3a3a6e7e2a7d829b523a07a
Erik Reyna <erik.reyna@washpost.com>
2016-02-08 21:52:57 -0500

    add active states on scroll; TODO: adjust for last hash triggering

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,6 +1,23 @@
 //= require_tree .
 
 $(document).ready(function () {
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('#js-navigation-menu a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#js-navigation-menu ul li a').removeClass("selected");
+                currLink.addClass("selected");
+            } else {
+                currLink.removeClass("selected");
+            }
+        });
+    }
+
+    $(document).on("scroll", onScroll);
+
     var menuToggle;
     menuToggle = $('#js-navigation-mobile-menu').unbind();
     $('#js-navigation-menu').removeClass('show');
@@ -13,6 +30,7 @@ $(document).ready(function () {
         });
     });
     $('#js-navigation-menu').find('a').click(function () {
+        $(document).off("scroll");
         var $href = $(this).attr('href');
         var $anchor = $($href).offset();
 
@@ -23,19 +41,19 @@ $(document).ready(function () {
         var sectionScrollTopFix = $anchor.top - parseInt(bodyPadding);
 
         $('body').animate({
-            scrollTop: sectionScrollTopFix,
-            duration: 500,
-            easing: 'easein'
+            scrollTop: sectionScrollTopFix
+        }, 500, 'swing', function () {
+             $(document).on("scroll", onScroll);
         });
 
         return false;
     });
 
 
-    $('#ss-form').submit( function(){
-       $(this).hide();
-       $('#crayhola-submit-page').show();
-       console.log("you submitted!")
+    $('#ss-form').submit(function () {
+        $(this).hide();
+        $('#crayhola-submit-page').show();
+        console.log("you submitted!")
     });
 
 
