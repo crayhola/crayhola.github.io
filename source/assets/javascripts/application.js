0dcdbfa42e8603b9ead32cd61202f97fb50dc59b
Aaron Williams <aaron.colby.williams@gmail.com>
2016-03-07 22:40:53 -0500

    button, new fonts, scrollTo func

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,6 +2,28 @@
 
 $(document).ready(function () {
 
+    function _scrollTo (node) {
+        $(document).off("scroll");
+        var $href = $(node).attr('href');
+        var $anchor = $($href).offset();
+
+        $('#js-navigation-menu').find('a').removeClass('selected');
+        $(this).addClass('selected');
+
+        var bodyPadding = $('section').css('margin-top');
+        var sectionScrollTopFix = ($anchor.top - 75) - parseInt(bodyPadding);
+
+        $('body').animate({
+            scrollTop: sectionScrollTopFix,
+            duration: 500,
+            easing: 'easein'
+        }, 500, 'swing', function () {
+             $(document).on("scroll", onScroll);
+        });
+
+        return false;
+    }
+
     function onScroll(event) {
         var scrollPos = $(document).scrollTop();
         $('#js-navigation-menu a').each(function () {
@@ -29,28 +51,12 @@ $(document).ready(function () {
             }
         });
     });
-    $('#js-navigation-menu').find('a').click(function () {
-        $(document).off("scroll");
-        var $href = $(this).attr('href');
-        var $anchor = $($href).offset();
-
-        $('#js-navigation-menu').find('a').removeClass('selected');
-        $(this).addClass('selected');
-
-        var bodyPadding = $('section').css('margin-top');
-        var sectionScrollTopFix = ($anchor.top - 75) - parseInt(bodyPadding);
-
-        $('body').animate({
-            scrollTop: sectionScrollTopFix,
-            duration: 500,
-            easing: 'easein'
-        }, 500, 'swing', function () {
-             $(document).on("scroll", onScroll);
-        });
-
-        return false;
+    $('#join-joc').on('click', function () {
+        _scrollTo(this);
     });
-
+    $('#js-navigation-menu').find('a').click(function () {
+        _scrollTo(this);
+    });
 
     $('#ss-form').submit(function () {
         $(this).hide();
